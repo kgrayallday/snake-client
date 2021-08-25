9b5d1f2ae75711e95ab0e480e6a7d81dbac1c542
kyl graym <kylemaxgray@gmail.com>
2021-08-25 21:20:54 +0000

    sssSSSsssSSSsss

---
+++ client.js
@@ -0,0 +1,36 @@
+const net = require("net");
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: '135.23.223.133', // IP address here,
+    port: 50542 // PORT number here,
+  });
+
+  // update the connect function to handle incoming data and console.log it for the player
+  conn.on("connect", () => {
+    console.log("what do i log here?")
+    // console.log(conn);
+  })
+
+  // logging data returned from the server? "you ded cuz you idled"
+  conn.on('data', (data) => {
+    console.log(data.toString());
+    conn.end();
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  // message logged to self when connection ends
+  conn.on('end', () => {
+  console.log('disconnected from server');
+  });
+
+  return conn;
+};
+
+module.exports = connect;
+
+module.exports = {
+  connect
+};
---
+++ play.js
@@ -0,0 +1,9 @@
+const net = require("net");
+// const playConnect = require('./play');
+const { connect } = require('./client');
+
+// establishes a connection with the game server
+
+
+console.log("Connecting ...");
+connect();
